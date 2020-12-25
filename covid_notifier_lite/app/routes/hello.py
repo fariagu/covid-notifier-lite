9d9a876ae01ba0148c45d705fb2a381265c65df9
Gustavo Faria <gustavoduartefaria@gmail.com>
2020-12-25 13:25:25 +0000

    added methods to read and write from database

---
+++ covid_notifier_lite/app/routes/hello.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Body
+from fastapi.encoders import jsonable_encoder
+
+from app.crud.hello import (
+    add_hello,
+    get_hello,
+    get_all_helloes,
+    update_hello,
+    delete_hello
+)
+
+from app.models.hello import (
+    ErrorResponseModel,
+    ResponseModel,
+    HelloSchema,
+    UpdateHelloModel
+)
+
+HelloRouter = APIRouter()
+
+@HelloRouter.post("/", response_description="Add hello data to database")
+async def controller_add_hello(hello: HelloSchema = Body(...)):
+    hello = jsonable_encoder(hello)
+    new_hello = await add_hello(hello)
+    return ResponseModel(new_hello, "Hello added successfully")
+
+@HelloRouter.get("/", response_description="Get all helloes from database")
+async def controller_get_all_helloes():
+    helloes = await get_all_helloes()
+    return ResponseModel(helloes, "Retrived Hello data")
