1e1169fd73f3854d25ad13d6f38973af64e45c56
Gustavo Faria <gustavoduartefaria@gmail.com>
2021-01-03 01:12:25 +0000

    Added db authentication and changed eol format to unix (#17)

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,16 @@
+from dotenv import load_dotenv
 from motor import motor_asyncio
 import os
+import urllib
 
+load_dotenv()
+
+db_username = os.environ.get('DB_USERNAME')
+db_password = urllib.parse.quote(os.environ.get('DB_PASSWORD'))
 db_container_name = os.environ.get('DB_CONTAINER_NAME')
 db_container_port = os.environ.get('DB_CONTAINER_PORT')
 
-MONGO_DETAILS = f"mongodb://{db_container_name}:{db_container_port}"
+MONGO_DETAILS = f"mongodb://{db_username}:{db_password}@{db_container_name}:{db_container_port}/?authSource=admin&readPreference=primary&ssl=false"
 
 client = motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
